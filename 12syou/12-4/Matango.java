d0d5b148a6edf523dbc9d3fd7a152b7d9b77a5b6
hotate1000 <ooo.336.sss@gmail.com>
2020-07-19 05:24:34 +0900

    12-4syou

---
+++ 12syou/12-4/Matango.java
@@ -0,0 +1,8 @@
+public class Matango {
+  int hp = 50;
+  char suffix;
+
+  public void Matango(char suffix) {
+    this.suffix = suffix;
+  }
+}
---
+++ 12syou/12-5/Main.java
@@ -0,0 +1,31 @@
+// public class Main {
+//   public static void main(String[] args) {
+//     Hero h1 = new Hero();
+//     Hero h2 = new Hero();
+//     Thief t1 = new Thief();
+//     Wizard w1 = new Wizard();
+//     Wizard w2 = new Wizard();
+
+//     h1.hp += 50;
+//     h2.hp += 50;
+//     t1.hp += 50;
+//     w1.hp += 50;
+//     w2.hp += 50;
+
+//   }
+// }
+
+public class Main {
+  public static void main(String[] args) {
+    Character[] c = new Character[5];
+    c[0] = new Hero();
+    c[1] = new Hero();
+    c[2] = new Hero();
+    c[3] = new Hero();
+    c[4] = new Hero();
+
+    for (Character ch:c) {
+      ch.hp += 50;
+    }
+  }
+}
