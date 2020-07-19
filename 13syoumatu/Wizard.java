9c03a039d975c607e2c991f65978bd51335561ac
hotate1000 <ooo.336.sss@gmail.com>
2020-07-19 23:24:55 +0900

    12-5syou

---
+++ 13syoumatu/Wizard.java
@@ -0,0 +1,20 @@
+public class Wizard {
+  private int hp;
+  private int mp;
+  private String name;
+  private Wand wond;
+
+  public void heal(Hero h) {
+    int basePoint = 10;
+    int recovPoint = (int)(basePoint * this.wand.power);
+    h.setHp(h.getHp() + recovPoint);
+    System.out.println(h.getName() + "のHPを" + recovPoint + "回復した！");
+  }
+    public int  getHp()       { return this.hp; }
+    public void setHp(int hp) { this.hp = hp; }
+    public int  getMp()       { return this.mp; }
+    public void setMp(int mp) { this.mp = mp; }
+    public String getName()   { return this.name;}
+    public void setName(String name) {this.wond = wond}
+}
+
