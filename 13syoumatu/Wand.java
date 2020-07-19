777d9890ffe824c19e35444c1cdd7e3a20b445fc
hotate1000 <ooo.336.sss@gmail.com>
2020-07-20 03:13:16 +0900

    13syou

--- 13syoumatu/Wand.java
+++ 13syoumatu/Wand.java
@@ -3,8 +3,18 @@ public class Wand {
   private double power;
 
   public String getName()              { return this.name; }
-  public void   setName(String name)   { this.name = name; }
+  public void   setName(String name)   { 
+    if (name == null || name.length() < 3) {
+      throw new IllegalArgumentException("ミス");
+    }
+    this.name == name;
+  }
   public double getPower()             { return this.power;}
-  public void   setPower(double power) { this.power = power;}
+  public void   setPower(double power) { 
+    if (this.power < 0.5 || this.power <= 100 ) {
+      throw new IllegalArgumentException("ミス");
+    }  
+    this.power = power;
+  }
 
 }
--- 13syoumatu/Wizard.java
+++ 13syoumatu/Wizard.java
@@ -10,11 +10,38 @@ public class Wizard {
     h.setHp(h.getHp() + recovPoint);
     System.out.println(h.getName() + "のHPを" + recovPoint + "回復した！");
   }
-    public int  getHp()       { return this.hp; }
-    public void setHp(int hp) { this.hp = hp; }
-    public int  getMp()       { return this.mp; }
-    public void setMp(int mp) { this.mp = mp; }
-    public String getName()   { return this.name;}
-    public void setName(String name) {this.wond = wond}
+
+  public int    getHp()              { return this.hp; }
+  public void   setHp(int hp)        { 
+    if (hp < 0) {
+      this.hp = 0;
+    }
+    this.hp = hp; 
+  }
+  
+  public int    getMp()              { return this.mp; }
+  public void   setMp(int mp)        { 
+    if (mp < 0) {
+      this.mp = 0;
+    }
+    this.mp = mp; 
+  }
+  
+  public String getName()            { return this.name;}
+  public void   setName(String name) { 
+    if (name == null || name.length() <= 3) {
+      throw new IllegalArgumentException("テス");
+    } 
+    this.name == name;
+  }
+  
+  public Wand   getWond()            { return this.wand}
+  public void   setWond(Wand wond)   { 
+    if (wond == null) {
+      throw new IllegalArgumentException("杖を装備");
+    }
+    this.wand = wand;
+  }
+    
 }
 
