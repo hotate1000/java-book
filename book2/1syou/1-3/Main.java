641a8e093ae4768d964437d6efad3e28df2c9f96
hotate1000 <ooo.336.sss@gmail.com>
2020-08-08 13:13:35 +0900

    1-2syou

--- book2/1syou/1-3/Main.java
+++ book2/1syou/1-3/Main.java
@@ -8,10 +8,11 @@ public class Main {
 
 
 
-    System.out.println("文字列s1にprogを含んでいるか" + s1.contains("prog"));
-    System.out.println("文字列s1がJaで始まるか" + s1.startsWith("Ja"));
-    System.out.println("文字列s1がingで終わるか" + s1.endsWith("ing"));
-    System.out.println("文字列s1のingが初めて登場する位置は" + s1.indexOf("ing"));
-    System.out.println("文字列s1のingが後ろから初めて登場する位置は" + s1.lastIndexOf("ing")); 
+    System.out.println("大文字を小文字に変換" + s1.toLowerCase());
+    System.out.println("小文字を大文字に変換" + s1.toUpperCase());
+    System.out.println("前後の空白を除去する" + s1.trim());
+
+    System.out.println("文字列を置き換える" + s1.replace("Java","test"));
+    System.out.println("文字列を置き換える" + s1.replace("Java",""));
   }
 }
