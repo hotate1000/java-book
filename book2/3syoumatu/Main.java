72ea74fd8a95b6375fe3c23b53dc59085e4beabe
hotate1000 <ooo.336.sss@gmail.com>
2020-08-12 22:41:22 +0900

    3syoumatu

---
+++ book2/3syoumatu/Main.java
@@ -0,0 +1,34 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    Hero h1 = new Hero();
+    Hero h2 = new Hero();
+    List<Hero> heroes = new ArrayList<Hero>();
+    heroes.add(h1);
+    heroes.add(h2);
+
+    for(Hero h : heroes) {
+      System.out.println(h.getName());
+    }
+  }
+  // public class Hero {
+  //   private String name;
+  //   public Hero(String name) {this.name = name;}
+  //   public String getName() {return this.name;}
+  // }
+}
+
+
+Hero h1 = new Hero("斎藤");
+Hero h2 = new Hero("鈴木");
+
+Map<Hero, Integer> heroes = new HashMap<Hero, Integer>();
+heroes.put(h1, 3);
+heroes.put(h2, 5);
+
+for(Hero key : heroes.keySet()) {
+  int value = heroes.get(key);
+  System.out.println(key.getName() + "が倒した敵=" + value);
+}
+
