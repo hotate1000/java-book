d4c784fd6da085e33443e6c2687d3bd006a605c9
hotate1000 <ooo.336.sss@gmail.com>
2020-08-29 12:23:21 +0900

    boo1 12syou

--- book1/12syou/Main.java
+++ book1/12syou/Main.java
@@ -12,3 +12,21 @@ public class Main {
     w.fireball(m);
   }
 }
+
+
+/* 
+public class Main {
+  public static void main(String[] args) {
+    Character[] c = new Charcter[];
+    c[0] = new Hero();
+    c[1] = new Hero();
+    c[2] = new Thief();
+    c[3] = new Wizard();
+    c[4] = new Wizard();
+
+    for(ch : c) {
+      ch.hp += 50;
+    }
+  }
+}
+*/
