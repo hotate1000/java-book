c3d4fd8b9f828881e3fe8d5b3b67daa74f71c5b8
hotate1000 <ooo.336.sss@gmail.com>
2020-09-02 13:43:26 +0900

    book1 15syoumatu

--- book1/15syoumatu/Main.java
+++ book1/15syoumatu/Main.java
@@ -1,61 +1,68 @@
-// public class Main {
-//   public static void main(String[] args) {
-//     StringBuilder sb = new StringBuilder();
-
-//     for(int i=0; i<100; i++) {
-//       sb.append(i+1).append(",");
-//     }
-//     String s = sb.toString();
-//     System.out.println(s);
-//   }
-
-
-//   public String concatPath(String folder,String file) {
-//     if(!folder.endsWith("¥¥")) {
-//       folder += "¥¥";
-//     }
-//     return folder + file;
-//   }
-// }
-
-
-// "Java".matches(".*");
-// "Appp".matches("A¥d{1,2}");
-// "USS".matches("U[a-zA-Z]{3}");
-
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import java.time.*;
-import java.time.format.*;
 import java.time.format.DateTimeFormatter;
-
-// public class Main {
-//   public static void main(String[] args) {
-//     Date now = new Date();
-//     Claender c = Calendar.getInstance();
-
-//     c.setTime(d);
-
-//     int day = c.get(Calendar.DAY_OF_MONTH);
-//     day += 100;
-
-//     c.set(Calender.DAY_OF_MONTH, day);
-
-//     Date future = c.getTime();
-
-//     SimpleDateFormat f = new SimpleDateFormat("西暦yyyy年MM月dd日");
-//     System.out.println(f.format(future));
-//   }
-// }
 
 
 public class Main {
+  // 15-1
   public static void main(String[] args) {
-    LocalDateTime now = LocalDateTime.now();
-    LocalDateTime future = now.plusDays(100);
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < 100 ; i++) {
+      // System.out.print(i + 1 + ",");
+      sb.append(i + 1).append(",");
+    }
 
-    DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+    String s = sb.toString();
+    String[] a = s.split(",");
+    System.out.println(a);
 
-    System.out.println(fmt);
-    System.out.println(future.format(fmt));
+
+  // 15-4
+    Date date  = new Date();
+
+    Calendar c = Calendar.getInstance();
+    c.setTime(date);
+    System.out.println(c);
+
+    int day    = c.get(Calendar.DAY_OF_MONTH);
+    System.out.println(day);
+
+    day = day + 100;
+    c.set(Calendar.DAY_OF_MONTH,day);
+
+    Date now   = c.getTime();
+
+    SimpleDateFormat f = new SimpleDateFormat("西暦yyyy年MM月dd日");
+    String sss = f.format(now);
+    System.out.println(sss);
+    
+
+  // 15-5
+    System.out.println("");
+    LocalDate nowa = LocalDate.now();
+    LocalDate futurea = nowa.plusDays(100);
+    System.out.println(futurea);
+
+    DateTimeFormatter fmta = DateTimeFormatter.ofPattern("西暦yyyy年MM月dd日");
+    System.out.println(futurea.format(fmta));
   }
+
+
+  // 15-2
+  // public String concatPath(String folder, String file) {
+  //   if(!folder.endWith("¥¥")) {
+  //     folder = folder + "¥¥";
+  //   }
+  //   return folder  + file;
+  // }
+
+
+  // 15-3
+    // (1).*
+    // (2)A¥d{1,2}
+    // (3)U[A-Z]{2,4}
+
 }
