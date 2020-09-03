5cf3d7fa2f0223c834e46f56dba23426dd19e6f6
hotate1000 <ooo.336.sss@gmail.com>
2020-09-03 16:17:23 +0900

    book1 17syou

--- book1/17syou/Main.java
+++ book1/17syou/Main.java
@@ -1,11 +1,38 @@
 import java.io.*;
 
+
 public class Main {
   public static void main(String[] args) {
+    FileWriter fw = null;
     try {
-      FileWriter fw = new FileWriter("data.txt");
-    } catch(IOException e) {
-      System.out.println("エラーが発生");
+      fw = new FileWriter("data.txt");
+      fw.write("hello1");
+      fw.write("hello1");
+      
+    } catch (Exception e) {
+      System.out.println("何かしらの例外が発生");
+    } finally {
+      if (fw != null) {
+        try {
+          fw.close(); 
+        } catch(IOException e) {
+          ;
+        }
+      }
+    }
+
+
+    try (FileWriter fw2 = new FileWriter("data2.txt");) {
+      fw2.write("hello2");
+    } catch (Exception e) {
+      System.out.println("何かしらの処理");
     }
   }
+
+  // public void setAge(int age) {
+  //   if (age < 0) {
+  //     throw new IllegalArgumentException("年齢は正の数を指定すべきです。");
+  //   }
+  //   this.age = age;
+  // }
 }
